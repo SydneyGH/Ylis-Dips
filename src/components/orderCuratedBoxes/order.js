0c46d15d945915df3469c582d095d3cd9258120d
Sydney Harrigan <sydgh17@gmail.com>
2024-05-06 22:37:21 -0400

    made edits to the Order page and restructured the files

--- src/components/orderCuratedBoxes/order.js
+++ src/components/orderCuratedBoxes/order.js
@@ -17,9 +17,19 @@ const Order = () => {
                         Strawberries:
                     </label>
                     <select className="sel-options">
+                        <option value="0"> </option>
                         <option value="1">Option1</option>
                         <option value="2">Option2</option>
                         <option value="3">Option3</option>
+                    </select>
+                    <label className="flavor1-label">
+                        Flavor:
+                    </label>
+                    <select className="sel-flavor">
+                        <option> </option>
+                        <option>Dark Chocolate</option>
+                        <option>Milk Chocolate</option>
+                        <option>Custon</option>
                     </select>
                 </div>
                 <div className="select-box"> 
@@ -27,9 +37,19 @@ const Order = () => {
                         Pretzels:
                     </label>
                     <select className="sel-options">
+                        <option value="0"> </option>
                         <option value="1">Option1</option>
                         <option value="2">Option2</option>
                         <option value="3">Option3</option>
+                    </select>
+                    <label className="flavor2-label">
+                        Flavor:
+                    </label>
+                    <select className="sel-flavor">
+                        <option> </option>
+                        <option>Dark Chocolate</option>
+                        <option>Milk Chocolate</option>
+                        <option>Custon</option>
                     </select>
                 </div>
                 <div className="select-box">
@@ -37,9 +57,19 @@ const Order = () => {
                         Oreos:
                     </label>
                     <select className="sel-options">
+                        <option value="0"> </option>
                         <option value="1">Option1</option>
                         <option value="2">Option2</option>
                         <option value="3">Option3</option>
+                    </select>
+                    <label className="flavor3-label">
+                        Flavor:
+                    </label>
+                    <select className="sel-flavor">
+                        <option> </option>
+                        <option>Dark Chocolate</option>
+                        <option>Milk Chocolate</option>
+                        <option>Custon</option>
                     </select>
                 </div>
                 <div>
